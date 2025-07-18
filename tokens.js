aeb080acc501604a0d2a61660b692d689aa2155c
hawsawi0077x <abdullahhawsawi0077x@gmail.com>
2025-07-18 21:24:33 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    channelId: "1393396660763099156",
-    serverId: "1243695238392840273",
+    channelId: "1386157633786085386",
+    serverId: "1346788139724963840",
     token: process.env.token1,
     selfDeaf: false,
     autoReconnect: {
@@ -15,8 +15,8 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1393396660763099156",
-    serverId: "1243695238392840273",
+    channelId: "1185755697024086106",
+    serverId: "1133667699151618130",
     token: process.env.token2,
     selfDeaf: false,
     autoReconnect: {
@@ -30,9 +30,39 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1393396660763099156",
-    serverId: "1243695238392840273",
+    channelId: "1394770446896861284",
+    serverId: "1346788139724963840",
     token: process.env.token3,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  }
+   {
+    channelId: "1394770446896861284",
+    serverId: "1346788139724963840",
+    token: process.env.token4,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  }
+   {
+    channelId: "1394770446896861284",
+    serverId: "1346788139724963840",
+    token: process.env.token5,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
