cab9089e8a5eddfcf5df808ae812ee62193e8e0a
hawsawi0077x <abdullahhawsawi0077x@gmail.com>
2025-08-09 15:14:11 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,9 +1,9 @@
 module.exports = [
   {
-    channelId: "1386157633786085386",
-    serverId: "1346788139724963840",
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
     token: process.env.token1,
-    selfDeaf: false,
+    selfDeaf: true,
     autoReconnect: {
       enabled: true,
       delay: 30000,
@@ -15,10 +15,10 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1185755697024086106",
-    serverId: "1133667699151618130", 
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196", 
     token: process.env.token2,
-    selfDeaf: false,
+    selfDeaf: true,
     autoReconnect: {
       enabled: true,
       delay: 30000,
@@ -30,10 +30,10 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1395923052545577040",
-    serverId: "1394436161182629998",
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
     token: process.env.token3,
-    selfDeaf: false,
+    selfDeaf: true,
     autoReconnect: {
       enabled: true,
       delay: 30000,
@@ -45,10 +45,10 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1394770446896861284",
-    serverId: "1346788139724963840",
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
     token: process.env.token4,
-    selfDeaf: false,
+    selfDeaf: true,
     autoReconnect: {
       enabled: true,
       delay: 30000,
@@ -60,10 +60,25 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1394770446896861284",
-    serverId: "1346788139724963840",
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
     token: process.env.token5,
-    selfDeaf: false,
+    selfDeaf: true,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  }
+  
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
+    token: process.env.token6,
+    selfDeaf: true,
     autoReconnect: {
       enabled: true,
       delay: 30000,
