a84d0030a229849b587f0bd14aa944aa66cd8a64
hawsawi0077x <abdullahhawsawi0077x@gmail.com>
2025-08-10 02:02:39 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -89,4 +89,19 @@ module.exports = [
     },
     selfMute: false,
   }
+    {
+    channelId: "1403712287470125200",
+    serverId: "1403712287470125196",
+    token: process.env.token7,
+    selfDeaf: true,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  }
 ];
